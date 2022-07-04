480ef9104a9bef4ac49de557611d98fe5a92d27e
Mikipaw <mapawlow@yandex.ru>
2022-07-05 01:20:53 +0300

    UDP client demo

--- UDPClient.py
+++ UDPClient.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
+from socket import *
+serverName = 'hostname'
+serverPort = 12000
 
+def udp_client():
+    message = raw_input('Input lowercase sentence:')
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    clientSocket.sendto(message, (serverName, serverPort))
+    modifiedMsg, serverAddr = clientSocket.recvfrom(2048)
+    print (modifiedMsg)
+    clientSocket.close()
