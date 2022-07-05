b49dfd6398babb734cf085e92a913694327d29c7
Mikipaw <mapawlow@yandex.ru>
2022-07-05 12:57:17 +0300

    instruction (-help) added

--- UDPClient.py
+++ UDPClient.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 from socket import *
+import sys
+
 #serverName = 'hostname'
 #serverPort = 12000
-bufferSize = 2048
+
+BUFFER_SIZE      = 2048
+PORT_LOWER_BOUND = 1024
+PORT_UPPER_BOUND = 5000
+
 
 def udp_client(serverName, serverPort):
     message = raw_input('Input lowercase sentence:')
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.sendto(message, (serverName, serverPort))
-    modifiedMsg, serverAddr = clientSocket.recvfrom(bufferSize)
+    modifiedMsg, serverAddr = clientSocket.recvfrom(BUFFER_SIZE)
     print (modifiedMsg)
     clientSocket.close()
 
+
+# The function checks user-entered parameters for client work
 def check_client_params(serverName, portNumber):
     if int(sys.argv[2]) > 5000 or int (sys.argv[2]) < 1024:
         print("Port number invalid. Port number should be in range (1024, 5000).")
@@ -23,33 +31,52 @@ def check_client_params(serverName, portNumber):
         return False
     return True
 
+
 def udp_server(serverPort):
     serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('', serverPort))
     print ("The server is ready to receive")
     while True:
-        message, clientAddr = serverSocket.recvfrom(bufferSize)
+        message, clientAddr = serverSocket.recvfrom(BUFFER_SIZE)
         modifiedMsg = message.upper()
         serverSocket.sendto(modifiedMsg, clientAddr)
-        
+
+
+# The function checks user-entered parameters for server work
 def check_server_params(serverPort):
-    if int(sys.argv[1]) > 5000 or int (sys.argv[2]) < 1024:
+    if int(sys.argv[1]) > 5000 or int (sys.argv[1]) < 1024:
         print("Port number invalid. Port number should be in range (1024, 5000).")
         return False
     return True        
+
+
+def PrintInformation():
+    print("This program maintains working like a server with UDP socket and like a client.")
+    print("If you want to use the program like a server, just enter the server port when you run a program. For example, \"python UDPClient 2048\"")
+    print("If you want to use the program like a client, just enter the server name and port number when you run a program. For example, \"python UDPClient hostname 2048\"")
+    print("Port number must be in range (1024, 5000) to correct execution")
+    return
         
+
 if __name__ == '__main__':
     if len (sys.argv) == 3:
         type = check_client_params(sys.argv[1], sys.argv[2]) #checks server name and port number
         if type:
-            udp_client(argv[1], int(argv[2]))
+            udp_client(sys.argv[1], int(sys.argv[2]))
         else:
             print("Invalid input")
-    elif len (sys.argv == 2): #checks port number
-        type = check_server_params()
+            print("Please, enter \"python UDPClient -help\" to get instructions")
+
+    elif len (sys.argv) == 2: #checks port number
+        type = check_server_params(sys.argv[1])
         if type:
-            udp_server(int(argv[1]))
+            udp_server(int(sys.argv[1]))
+        elif sys.argv[1] == "-help":
+            PrintInformation()
         else:
             print("Invalid input")
+            print("Please, enter \"python UDPClient -help\" to get instructions")
+
     else:
-        print ("Invalid input! The programm expects 1 or 2 params, but {} were given").format(len(sys.argv) - 1)
+        print("Invalid input! The programm expects 1 or 2 params, but {} were given").format(len(sys.argv) - 1)
+        print("Please, enter \"python UDPClient -help\" to get instructions")
